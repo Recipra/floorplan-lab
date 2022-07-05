98364bdcf14554da3f1c79f1355520bfb325e4a4
recipra <jmo0425@outlook.com>
2022-07-05 16:14:37 -0400

    make floor plan and kitchen

---
+++ src/FloorPlan.jsx
@@ -0,0 +1,11 @@
+import Kitchen from "./Kitchen"
+
+const FloorPlan = (props) => {
+  return (
+    <>
+      <Kitchen />
+    </>
+  )
+}
+
+export default FloorPlan
---
+++ src/Kitchen.jsx
@@ -0,0 +1,9 @@
+const Kitchen = (props) => {
+  return (
+    <div>
+      <h1>This is the kitchen!</h1>
+    </div>
+  )
+}
+
+export default Kitchen
