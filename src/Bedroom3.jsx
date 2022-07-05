d0c15183efe6e0d00f07e3d5b9dd7aea1cd1d4ec
recipra <jmo0425@outlook.com>
2022-07-05 17:32:15 -0400

    add bedrooms

---
+++ src/Bedroom3.jsx
@@ -0,0 +1,9 @@
+const Bedroom3 = (props) => {
+  return (
+    <div>
+      <h1>Bedroom {props.bedNum}</h1>
+    </div>
+  )
+}
+
+export default Bedroom3
--- src/FloorPlan.jsx
+++ src/FloorPlan.jsx
@@ -1,11 +1,17 @@
+import Bedroom1 from "./Bedroom1"
+import Bedroom2 from "./Bedroom2"
+import Bedroom3 from "./Bedroom3"
 import Kitchen from "./Kitchen"
 import LivingRoom from "./LivingRoom"
 
 const FloorPlan = (props) => {
   return (
     <>
-      <Kitchen />
       <LivingRoom />
+      <Bedroom1 bedNum={1}/>
+      <Kitchen />
+      <Bedroom2 bedNum={2}/>
+      <Bedroom3 bedNum={3}/>
     </>
   )
 }
