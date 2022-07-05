ed586b5a1b708d26d3a5f28e7290961b7da6d5b5
recipra <jmo0425@outlook.com>
2022-07-05 17:35:47 -0400

    add baths

--- src/FloorPlan.jsx
+++ src/FloorPlan.jsx
@@ -1,6 +1,8 @@
 import Bedroom1 from "./Bedroom1"
 import Bedroom2 from "./Bedroom2"
 import Bedroom3 from "./Bedroom3"
+import FullBath from "./FullBath"
+import HalfBath from "./HalfBath"
 import Kitchen from "./Kitchen"
 import LivingRoom from "./LivingRoom"
 
@@ -9,8 +11,10 @@ const FloorPlan = (props) => {
     <>
       <LivingRoom />
       <Bedroom1 bedNum={1}/>
+      <HalfBath size={'Half'}/>
       <Kitchen />
       <Bedroom2 bedNum={2}/>
+      <FullBath size={'Full'}/>
       <Bedroom3 bedNum={3}/>
     </>
   )
